0888449bd391ac9f50f54753316ed4ad6081269e
Jens <j.kunze@uq.edu.au>
2024-10-25 10:20:29 +1000

    added add_noise function

--- tools.lua
+++ tools.lua
@@ -270,6 +270,11 @@ local function vec_magnitude(vec)
     return mag^0.5
 end
 
+local function add_noise(noise_magnitude)
+    math.randomseed(os.time())
+    return 1 + noise_magnitude * (math.random() - 0.5)
+end
+
 return {mean = mean, 
     sum = sum, 
     round = round, 
@@ -295,5 +300,6 @@ return {mean = mean,
     ellipse_circumference = ellipse_circumference,
     calculate_centroid = calculate_centroid,
     compare_points = compare_points,
-    vec_magnitude = vec_magnitude
+    vec_magnitude = vec_magnitude,
+    add_noise = add_noise
     }
